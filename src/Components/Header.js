91429bcedf136fbc16e2779ed8e95ece71fb3af4
Tiril Fjell <tiril.fjell@gmail.com>
2021-06-01 17:19:05 +0200

    typewriter animation on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
+import TypeWriterEffect from "react-typewriter-effect";
+
 class Header extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
       var occupation = this.props.data.occupation;
       var description = this.props.data.description;
       var city = this.props.data.address.city;
@@ -64,7 +65,23 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
+            <h1 className="responsive-headline">
+              <TypeWriterEffect
+                textStyle={{
+                  display: "inline-block",
+                  verticalAlign: "middle",
+                  margin: 0,
+                  width: "85%",
+                  paddingBottom: 30,
+                  textAlign: "center"
+                }}
+                startDelay={100}
+                cursorColor="black"
+                text="I'm Tiril Fjell."
+                typeSpeed={100}
+              />
+            </h1>
+
             <h3>
               I'm a {city} based <span>{occupation}</span>. {description}.
             </h3>
