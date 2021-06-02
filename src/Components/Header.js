6fa96eada7649ee9e3e3ea5a7c38be8985dcc115
Tiril Fjell <tiril.fjell@gmail.com>
2021-06-02 18:16:40 +0200

    check ups

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -47,7 +47,7 @@ class Header extends Component {
             </li>
             <li>
               <a className="smoothscroll" href="#resume">
-                Resume
+                Skills
               </a>
             </li>
             <li>
