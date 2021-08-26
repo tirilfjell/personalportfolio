aade31bb926aa4dcabd7e6167fc818c207f8a6d0
Tiril Fjell <tiril.fjell@gmail.com>
2021-08-26 13:50:52 +0200

    form fix

--- src/Components/ContactForm.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function ContactForm() {
-  return (
-    <div>
-      <form name="contact" method="post" data-netlify="true"></form>
-    </div>
-  );
-}
